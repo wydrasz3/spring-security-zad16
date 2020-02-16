1d715d4cf5aeeb943ba60a53dfb00d9cae3fcd10
MSI <wydrasz3@gmail.com>
2020-02-16 13:31:29 +0100

    skanowanie encji i repozytoriów

--- src/main/java/pl/sda/zad16/config/Zad16Application.java
+++ src/main/java/pl/sda/zad16/config/Zad16Application.java
@@ -2,12 +2,16 @@ package pl.sda.zad16.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @ComponentScan("pl.sda.zad16")
 @Import(SecurityConfig.class)
+@EnableJpaRepositories("pl.sda.zad16.repository")
+@EntityScan("pl.sda.zad16.model")
 public class Zad16Application {
 
 	public static void main(String[] args) {
