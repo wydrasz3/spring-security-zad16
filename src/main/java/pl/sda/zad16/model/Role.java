9ebdc4f04b345ffbc20b674154797d62b6764b17
MSI <wydrasz3@gmail.com>
2020-02-16 13:45:32 +0100

    encje

--- src/main/java/pl/sda/zad16/model/Role.java
+++ src/main/java/pl/sda/zad16/model/Role.java
@@ -1,7 +1,15 @@
 package pl.sda.zad16.model;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
 
 @Entity
 public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
+    private Long id;
+
+    @Column
+    private String name;
 }
--- src/main/java/pl/sda/zad16/model/User.java
+++ src/main/java/pl/sda/zad16/model/User.java
@@ -1,6 +1,7 @@
 package pl.sda.zad16.model;
 
 import javax.persistence.*;
+import java.util.Set;
 
 @Entity
 public class User {
@@ -10,5 +11,21 @@ public class User {
     @Column
     private Long id;
 
+    @Column
+    private String username;
+
+    @Column
+    private String password;
+
+    @Column
+    private boolean enabled;
+
+    @ManyToMany(cascade = {CascadeType.ALL})
+    @JoinTable(
+            name = "user_role",
+            joinColumns = {@JoinColumn(name = "user_id")},
+            inverseJoinColumns = {@JoinColumn(name = "role_id")}
+    )
+    private Set<Role> roles;
 
 }
