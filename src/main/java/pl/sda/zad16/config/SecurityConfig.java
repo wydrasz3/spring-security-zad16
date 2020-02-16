ceb3353695e9edf7274b0cd6bc64a15d1f932961
MSI <wydrasz3@gmail.com>
2020-02-16 12:50:35 +0100

    main page url

--- src/main/java/pl/sda/zad16/config/SecurityConfig.java
+++ src/main/java/pl/sda/zad16/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package pl.sda.zad16.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -7,9 +8,14 @@ import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+import javax.sql.DataSource;
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private DataSource dataSource;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -42,6 +48,20 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .withUser("admin@wp.pl")
                 .password("{noop}admin123")
                 .roles("USER", "ADMIN");
+
+        auth
+                .jdbcAuthentication()
+                .dataSource(dataSource)
+                .usersByUsernameQuery(getUserQuery())
+                .authoritiesByUsernameQuery(getAuthoritiesQuery());
+    }
+
+    private String getAuthoritiesQuery() {
+        return "";
+    }
+
+    private String getUserQuery() {
+        return "";
     }
 
     @Override
